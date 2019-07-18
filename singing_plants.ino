80a50b8679d49fe0c678577aea94df164f0389ba
Erich Flock <e.flock@tdsoftware.de>
2019-07-18 22:04:04 +0200

    refactor - improve logic

--- singing_plants.ino
+++ singing_plants.ino
@@ -4,38 +4,33 @@
 CapacitiveSensor sens = CapacitiveSensor(4, 3); // 10M resistor between pins 4 & 3, pin 3 is sensor pin, add a wire
 
 #include <Average.h>
+Average<float> sensorValuesAverage(5);
 
-int y;
-float sensorValues[5];
-int index = 0;
+float differenceThreshold = 100.0;
+int count = 0;
+int countLimit = 5;
 
 void setup()
 {
-  //sens.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-
   Serial.begin(9600);
+  
   //for (int i = 500; i > 0; i--) Serial.println(0);
 }
 
 void loop()
 {
-  long start = millis();
   float sensorValue =  sens.capacitiveSensor(80);
+
+  sensorValuesAverage.push(sensorValue);
   
-  //Serial.println((String)"Sensor Value: " + sensorValue);
+  Serial.println((String)"Sensor Value: " + sensorValue);
 
   // the following logic was added in order to avoid a strange behavior in the sensor after being touched. The same or a similar value is read multiple times, 
   // making the protoype to play the same sound repetitively
   
-  if (index <= 4) {
-    sensorValues[index] = sensorValue;
-    index = index + 1;
-  } else {
-    if (hasCrashed()) {
-      //Serial.println("Sensor Crashed - Reseting it");
-      sens.reset_CS_AutoCal();
-    }
-    index = 0;
+  if (hasCrashed(sensorValue)) {
+    //Serial.println("Sensor Crashed - Reseting it");
+    sens.reset_CS_AutoCal();
   }
   
   if (sensorValue > 300) {
@@ -54,19 +49,23 @@ void loop()
   delay(1);
 }
 
-bool hasCrashed() {
+bool hasCrashed(float sensorValue) {
 
-  Average<float> sensorValuesAverage(5);
-  
-  for(int i = 0; i < 5; i++) {
-    sensorValuesAverage.push(sensorValues[i]);
-  }
-
-  float difference = abs(sensorValuesAverage.mean() - sensorValues[4]);
-  
-  if (difference < 100) {
-    return true;
+  if (count < countLimit) {
+    
+    count = count + 1;
+    return false;
+    
   } else {
-    return false; 
+
+    count = 0; //reset count
+    
+    float difference = abs(sensorValuesAverage.mean() - sensorValue);
+    
+    if (difference < differenceThreshold) {
+      return true;
+    } else {
+      return false; 
+    } 
   }
 }
